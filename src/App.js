5091eaa95b197935db90dbca82cf00a80f06446c
Pulkit <297pulkitmehta@gmail.com>
2022-10-12 17:44:37 -0600

    setting up routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
-import { BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import Quiz from "./pages/Quiz/Quiz";
+import Result from "./pages/Result/Result";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app" style={{ backgroundImage: "url(./ques1.png)" }}>
         <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/quiz" element={<Quiz />}></Route>
+          <Route path="/result" element={<Result />}></Route>
+        </Routes>
       </div>
+
+      <Footer />
     </BrowserRouter>
   );
 }
